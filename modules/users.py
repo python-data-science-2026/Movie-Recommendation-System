274c043d67035402429083804cbb01dc9a911f9a
Moise Christian Junior MEKA <moisechristianjunior464@gmail.com>
2026-04-28 11:17:13 +0200

    Adding user preferencies and get all of those.

--- modules/users.py
+++ modules/users.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 import pandas as pd 
 import hashlib
+from .genres import Genre
+from .actors import Actors
 
 PROJECT_ROOT = Path(__file__).parent.parent
 DATA_PATH = PROJECT_ROOT / "data" / "users.csv"
+GENRE_PREF_DATA_PATH = PROJECT_ROOT / "data" / "user_genre.csv"
+ACTORS_PREF_DATA_PATH = PROJECT_ROOT / "data" / "user_actors.csv"
 
 ##=============================================
 ## User module : To manage users information 
@@ -11,6 +15,9 @@ DATA_PATH = PROJECT_ROOT / "data" / "users.csv"
 class User:
     def __init__(self, username: str, password: str, lastname: str = '', firstname: str = None, date_of_birth: str = None):
         self.all_users = pd.read_csv(DATA_PATH, index_col="username")
+        self.all_genre_preferencies = pd.read_csv(GENRE_PREF_DATA_PATH)
+        self.all_actors_preferencies = pd.read_csv(ACTORS_PREF_DATA_PATH)
+
         self.username = username.strip()
         self.lastname = lastname.strip()
         self.firstname = firstname.strip() if firstname else None
@@ -59,3 +66,44 @@ class User:
         self.all_users.to_csv(DATA_PATH)
         return True
 
+    def add_genre_preferencies(self, genre:str):
+        genre_object = Genre(genre)
+        _ = genre_object.save()
+
+        filtered_row = self.all_genre_preferencies[(self.all_genre_preferencies['username'] == self.username)&
+                                                    (self.all_genre_preferencies['genre_id'] == genre_object.get_id())]
+        if len(filtered_row) == 0:
+            new_row = pd.DataFrame({
+                'username':self.username,
+                'genre_id' : genre_object.get_id()
+            })
+
+            self.all_genre_preferencies = pd.concat([self.all_genre_preferencies, new_row])
+            self.all_genre_preferencies.to_csv(GENRE_PREF_DATA_PATH)
+            return True
+        return False
+
+
+    def add_actors_preferencies(self, actor:str):
+        actor_object = Actors(actor)
+        _ = actor_object.save()
+
+        filtered_row = self.all_actors_preferencies[(self.all_actors_preferencies['username'] == self.username)&
+                                                    (self.all_actors_preferencies['actor_id'] == actor_object.get_id())]
+
+        if len(filtered_row) == 0:
+            new_row = pd.DataFrame({
+                'username':self.username,
+                'actor_id' : actor_object.get_id()
+            })
+
+            self.all_actors_preferencies = pd.concat([self.all_actors_preferencies, new_row])
+            self.all_actors_preferencies.to_csv(ACTORS_PREF_DATA_PATH)
+            return True
+        return False
+    
+    def get_genre_preferencies(self):
+        return  self.all_genre_preferencies[(self.all_genre_preferencies['username'] == self.username)]
+
+    def get_actors_preferencies(self):
+        return self.all_actors_preferencies[self.all_actors_preferencies['username'] == self.username]
